d1e511ab644ce984ff1c51f1d316cd74cdb94c56
nicolacaddeo <nicola.caddeo58@gmail.com>
2022-09-21 22:35:11 +0200

    created AppTitle component

---
+++ vue-cli-first/src/components/AppTitle.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <h1>La mia prima app con Vue CLI!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AppTitle',
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
